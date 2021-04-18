109d24f66463ec98b6e4f1548e9fa1522f08df39
hex <72889112+hex3928@users.noreply.github.com>
2021-04-18 15:31:03 +0200

    Update deps.ts

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
-export { printf, sprintf } from "https://deno.land/std@0.93.0/fmt/printf.ts"
+export { printf } from "https://deno.land/std@0.93.0/fmt/printf.ts"
 export * as colors from "https://deno.land/std@0.93.0/fmt/colors.ts"
